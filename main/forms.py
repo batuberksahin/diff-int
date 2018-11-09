dbdf901b9f412304fd19196db7d39c637f546180
Batu Berk <batuberkshn@gmail.com>
2018-11-09 21:58:36 +0300

    theme edit

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,1 @@
 from django import forms
-from .models import question
-
-class questionForms(forms.Form):
-    value = forms.CharField(label='Denklem', required=False)
-    variable = forms.CharField(label='Degişken', required=False)
-
-    variable.clean('x')
-    """class Meta:
-        model = question
-        fields = [
-            'value',
-            'variable',
-        ]"""
--- main/views.py
+++ main/views.py
@@ -1,47 +1,44 @@
 from django.shortcuts import render
-from .forms import questionForms
-from .models import question
 import sympy
 
-def index_page(request):
+def page(request):
 
     flag = 0
+    equation = ''
+
+    value = request.POST.get('value')
+    variable = request.POST.get('variable')
 
     if request.method == "POST":
 
-        value = request.POST.get('value')
-        variable = request.POST.get('variable')
-        diffequation = ''
-
-        form = questionForms({'value': value, 'variable': variable})
-
+        # Türev veya İntegral alma
         if value and variable:
             if request.POST.get('action') == "Türev al":
                 try:
-                    diffequation = sympy.latex(sympy.diff(value, variable))
+                    equation = sympy.latex(sympy.diff(value, variable))
                 except:
                     flag = 1
             elif request.POST.get('action') == "İntegral al":
                 try:
-                    diffequation = sympy.latex(sympy.integrate(value, (variable,variable)))
+                    equation = sympy.latex(sympy.integrate(value, (variable,variable)))
                 except:
                     flag = 2
         else:
             flag = 3
 
         context = {
-            'form': form,
             'value': value,
-            'diff': diffequation,
+            'diff': equation,
             'flag': flag,
+            'variable': variable
         }
 
     else:
-
-        form = questionForms({'value': '', 'variable': 'x'})
-
         context = {
-            'form': form,
+            'value': value,
+            'diff': equation,
+            'flag': flag,
+            'variable': 'x',
         }
 
     return render(request, 'base.html', context)
